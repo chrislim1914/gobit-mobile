e6993bf0303e17d5ea1f202e8c140125518c4098
Daniel Sarabusing <daniels.uth702@gmail.com>
2019-04-24 10:26:45 +0800

    added global/footer

---
+++ src/components/Globals/Footer.vue
@@ -0,0 +1,16 @@
+<template>
+  <!-- footer -->
+  <div class="app-footer">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'GlobalFooter'
+}
+</script>
+
+<style lang='scss' scoped>
+  @import '~scss/footer/base.scss';
+</style>
