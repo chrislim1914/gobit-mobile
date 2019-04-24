2d5fde44e5c024c199ef7d9b37f61087579466f2
Daniel Sarabusing <daniels.uth702@gmail.com>
2019-04-24 16:39:20 +0800

    added exchange ticker bar

--- src/components/Exchange/Index.vue
+++ src/components/Exchange/Index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="app-exchange">
-    <!-- search -->
     <div class="container-fluid">
+      <!-- search -->
       <div id="exchange-searchbar-row" class="row">
         <div class="col-12 pt-2">
           <label class="sr-only" for="exchange-searchbar-field">코인명 검색</label>
@@ -12,6 +12,51 @@
               </div>
             </div>
             <input type="text" class="form-control" id="exchange-searchbar-field" placeholder="코인명 검색">
+          </div>
+        </div>
+      </div>
+      <!-- tickerbar -->
+      <div id="exchange-tickerbar-row" class="row">
+        <div class="col-12">
+          <div class="container-fluid">
+            <div id="exchange-tickerbar-content-row" class="row">
+              <div class="col-6">
+                <div class="row">
+                  <div class="col-6">
+                    총 매수
+                  </div>
+                  <div class="col-6 text-right">
+                    <h6>266,481</h6>
+                  </div>
+                </div>
+                <div class="row">
+                  <div class="col-6">
+                    총 평가
+                  </div>
+                  <div class="col-6 text-right">
+                    <h6>129,544</h6>
+                  </div>
+                </div>
+              </div>
+              <div class="col-6">
+                <div class="row">
+                  <div class="col-6">
+                    평가손익
+                  </div>
+                  <div class="col-6 text-right">
+                   <h6 class="exchange-tickerbar-right-values">-136,936</h6>
+                  </div>
+                </div>
+                <div class="row">
+                  <div class="col-6">
+                    수익률
+                  </div>
+                  <div class="col-6 text-right">
+                    <h6 class="exchange-tickerbar-right-values">-51.39%</h6>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
