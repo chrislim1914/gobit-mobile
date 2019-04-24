7ab6eec7b8311a92e9f5d036189d43aeb6839072
Daniel Sarabusing <daniels.uth702@gmail.com>
2019-04-24 15:21:27 +0800

    updated customer service

--- src/components/Exchange/Index.vue
+++ src/components/Exchange/Index.vue
@@ -1,17 +1,22 @@
 <template>
   <div class="app-exchange">
 
+    <!-- extra -->
+    <div id="extra-colored-space"></div>
     <!-- globals -->
+    <global-cs></global-cs>
     <global-footer></global-footer>
   </div>
 </template>
 
 <script>
+import CustomerService from '../Globals/CustomerService'
 import Footer from '../Globals/Footer'
 export default {
   name: 'Exchange',
   components: {
-    'global-footer': Footer
+    'global-footer': Footer,
+    'global-cs': CustomerService
   }
 }
 </script>
--- src/components/Globals/CustomerService.vue
+++ src/components/Globals/CustomerService.vue
@@ -1,6 +1,35 @@
 <template>
+  <!-- custmer service -->
   <div class="app-customerservice">
-
+    <div class="container-fluid">
+      <div id="customer-service-header-row" class="row border-bottom">
+        <div class="col-12">
+          <h1 id="customer-service-header" class="font-weight-bold pt-3">
+            고객센터
+          </h1>
+        </div>
+      </div>
+      <div id="customer-service-link-row" class="row text-center">
+        <div class="col-4 border-right">
+          <h1 class="customer-service-link-text">
+            <i class="fa-2x fa fa-phone pb-2"></i><br/>
+            1544-0000
+          </h1>
+        </div>
+        <div class="col-4 border-right">
+          <h1 class="customer-service-link-text">
+            <i class="fa-2x fa fa-comment pb-2"></i><br/>
+            카톡 상담
+          </h1>
+        </div>
+        <div class="col-4">
+          <h1 class="customer-service-link-text">
+            <i class="fa-2x fa fa-download pb-2"></i><br/>
+            App 다운
+          </h1>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
