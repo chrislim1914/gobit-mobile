bea89c494d3a609b00f9d4562bbbc2e13e73f145
Daniel Sarabusing <daniels.uth702@gmail.com>
2019-04-25 15:23:12 +0800

    updated global header

--- src/components/Globals/Header.vue
+++ src/components/Globals/Header.vue
@@ -1,43 +1,49 @@
 <template>
   <!-- header -->
   <div class="app-header">
-      <div class="logo">
-        <img src="~img/gobit-logo.png" alt="gobit logo">
+    <div class="container-fluid">
+      <div id="header-row" class="row">
+        <div class="col-4">
+          <div class="logo">
+            <img src="~img/gobit-logo.png" alt="gobit logo" class="img-fluid">
+          </div>
+        </div>
+        <div class="col-8 text-right">
+          <ul class="list-inline group-header">
+            <li id="language-list" class="list-inline-item menu">
+              <a href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" class="nav-link dropdown-toggle">
+                <span class="text-uppercase">
+                  <img src="~img/south-korea.png" alt="gobit logo"> KOR
+                </span>
+              </a>
+              <div aria-labelledby="navbarDropdown" class="dropdown-menu">
+                <a href="#" class="dropdown-item text-uppercase text-left">
+                  <img src="~img/united-states.png" alt="gobit logo"> US
+                  </a>
+                <a href="#" class="dropdown-item text-uppercase text-left">
+                  <img src="~img/china.png" alt="gobit logo"> CHN
+                  </a>
+              </div>
+            </li>
+            <li class="list-inline-item">
+              <a href="#">
+                로그인
+              </a>
+            </li>
+            <li class="list-inline-item">
+              <a href="#">
+                회원가입
+              </a>
+            </li>
+            <li class="list-inline-item menu">
+              <a href="#">
+                <img src="~img/menu.png" data-effect="sidebar" alt="gobit logo" class="img-fluid">
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
-      <div class="group-header">
-        <ul class="inline">
-          <li id="language-list" class="list-inline-item menu">
-            <a href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" class="nav-link dropdown-toggle">
-              <span class="text-uppercase">
-                <img src="~img/south-korea.png" alt="gobit logo"> KOR
-              </span>
-            </a>
-            <div aria-labelledby="navbarDropdown" class="dropdown-menu">
-              <a href="#" class="dropdown-item text-uppercase text-left">
-                <img src="~img/united-states.png" alt="gobit logo"> US
-                </a>
-              <a href="#" class="dropdown-item text-uppercase text-left">
-                <img src="~img/china.png" alt="gobit logo"> CHN
-                </a>
-            </div>
-          </li>
-          <li class="list-inline-item">
-            <a href="#">
-              로그인
-            </a>
-          </li>
-          <li class="list-inline-item">
-            <a href="#">
-              회원가입
-            </a>
-          </li>
-          <li class="list-inline-item menu">
-            <a href="#">
-              <img src="~img/menu.png" data-effect="sidebar" alt="gobit logo">
-            </a>
-          </li>
-        </ul>
-      </div>
+    </div>
   </div>
 </template>
 
