60c2a8e2c2270d0aec30b37087c34b308961c198
Daniel Sarabusing <daniels.uth702@gmail.com>
2019-04-26 11:48:14 +0800

    created trading component, added topbar

---
+++ src/components/Trading/Index.vue
@@ -0,0 +1,20 @@
+<template>
+  <!-- trading -->
+  <div class="app-trading">
+    <topbar></topbar>
+  </div>
+</template>
+
+<script>
+import Topbar from './Topbar'
+export default {
+  name: 'Trading',
+  components: {
+    'topbar': Topbar
+  }
+}
+</script>
+
+<style lang='scss' scoped>
+  @import '~scss/trading/base.scss';
+</style>
