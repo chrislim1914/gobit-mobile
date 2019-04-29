e5258f686a882d38342af9990331602ff84a9284
Daniel Sarabusing <daniels.uth702@gmail.com>
2019-04-29 14:23:06 +0800

    added sign up verification message

--- src/components/Signup/EmailVerification.vue
+++ src/components/Signup/EmailVerification.vue
@@ -1,7 +1,7 @@
 <template>
   <!-- signup verification -->
   <div class="app-signup-verification">
-    <div class="container-fluid px-0 py-0">
+    <div id="signup-verification-fields" class="container-fluid px-0 py-0 d-none">
       <div id="signup-verification-row" class="row">
         <div class="col-12">
           <h6 id="signup-title">
@@ -27,6 +27,38 @@
             <a href="#" id="signup-signin-link">
               로그인 하기
             </a>
+          </h6>
+        </div>
+      </div>
+    </div>
+    <div id="signup-verification-message" class="container-fluid px-0 py-0">
+      <div id="signup-verification-row" class="row">
+        <div class="col-12">
+          <h6 id="signup-title">
+            회원가입
+          </h6>
+        </div>
+        <div class="col-12">
+          <h6 id="signup-email">
+            soonsill2@gmail.com
+          </h6>
+        </div>
+        <div id="signup-message-column" class="col-12">
+          <h6 id="signup-message-1">
+            인증 메일이 발송 되었습니다
+          </h6>
+          <h6 id="signup-message-2">
+            (인증 유효 시간은 30분 입니다)
+          </h6>
+          <h6 id="signup-message-3">
+            입력하신 이메일에서 [이메일 인증하기] 링크를 클릭하여<br/>회원가입을 진행 해 주세요
+          </h6>
+          <h6 id="signup-message-4">
+            인증메일이 도착하지않을 경우 스팸메일함을 확인하시거나,<br/>다시시도를 해주세요
+          </h6>
+          <h6 id="signup-message-5">
+            인증 메일 수신이 되지 않는 다면<br/>
+            <a href="mailto:gobit@gobit.me">Gobit@gobit.me로</a> 문의해 주시기 바랍니다.
           </h6>
         </div>
       </div>
