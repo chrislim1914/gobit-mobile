1b8789cb0d26c33120ea4661b0b1e37b037fdd79
Daniel Sarabusing <daniels.uth702@gmail.com>
2019-04-30 15:04:44 +0800

    updated login

--- src/components/Login/Index.vue
+++ src/components/Login/Index.vue
@@ -2,41 +2,41 @@
   <!-- login -->
   <div class="app-login">
     <!-- login header -->
-    <login-header id="login-header"></login-header>
+    <login-header></login-header>
     <!-- -->
-    <hr id="login-header-bottom-border">
+    <hr id="login-header-bottom-border" class="my-0 mx-0">
     <!-- -->
     <div id="login-container" class="container-fluid">
       <div id="login-title-row" class="row">
         <div class="col-12">
-          <h6 id="login-title">
+          <h6 id="login-title" class="font-weight-bold">
             로그인
           </h6>
         </div>
       </div>
       <div id="login-fields-row" class="row">
         <div class="col-12">
-          <input type="email" id="login-email-field" class="form-control"  placeholder="이메일을 입력해 주세요">
+          <input type="email" id="login-email-field" class="form-control font-weight-bold"  placeholder="이메일을 입력해 주세요">
         </div>
         <div class="col-12">
-          <input type="password" id="login-password-field" class="form-control"  placeholder="비밀번호를 입력해 주세요">
+          <input type="password" id="login-password-field" class="form-control font-weight-bold"  placeholder="비밀번호를 입력해 주세요">
         </div>
         <div class="col-12">
-          <button type="submit" id="login-button" class="btn">
+          <button type="submit" id="login-button" class="btn font-weight-bold">
             로그인
           </button>
         </div>
       </div>
-      <div id="login-links-row" class="row text-center">
-        <div class="col-5">
+      <div id="login-links-row" class="row font-weight-bold">
+        <div class="col-5 text-right">
           <a href="#" id="login-link-1">
             회원가입
           </a>
         </div>
-        <div class="col-1">
-          <h6>|</h6>
+        <div class="col-0 text-center mx-0 px-0">
+          <h6 id="login-link-line">|</h6>
         </div>
-        <div class="col-5">
+        <div class="col-5 text-left">
           <a href="#" id="login-link-2">
             비밀번호 찾기
           </a>
