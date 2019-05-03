d8ccc101a6ca05814e5df13a7701957ffc7f5485
Daniel Sarabusing <daniels.uth702@gmail.com>
2019-05-03 17:35:46 +0800

    updated exchange table body

--- src/components/Exchange/Table.vue
+++ src/components/Exchange/Table.vue
@@ -4,33 +4,74 @@
     <div id="exchange-table-holder">
       <div id="exchange-table-content">
         <table id="exchange-table" cellpadding="0" cellspacing="0">
-          <thead>
-            <tr>
-              <th class="position-relative">
-                지수
-                <img src="~img/double-arrow.png" alt="Gobit table header arrow" class="img-fluid position-absolute">
-              </th>
-              <th class="position-relative">
-                한글명
-                <img src="~img/opposite-arrow.png" alt="Gobit table header arrow" class="img-fluid position-absolute">
-              </th>
-              <th class="position-relative">
-                현재가
-                <img src="~img/double-arrow.png" alt="Gobit table header arrow" class="img-fluid position-absolute">
-              </th>
-              <th class="position-relative">
-                전일대비
-                <img src="~img/double-arrow.png" alt="Gobit table header arrow" class="img-fluid position-absolute">
-              </th>
-              <th class="position-relative">
-                거래대금
-                <img src="~img/double-arrow.png" alt="Gobit table header arrow" class="img-fluid position-absolute">
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-
-          </tbody>
+          <tr id="exchange-table-head-row">
+            <th class="position-relative">
+              지수
+              <img src="~img/double-arrow.png" alt="Gobit table header arrow" class="img-fluid position-absolute" style="top:17px;">
+            </th>
+            <th class="position-relative">
+              한글명
+              <img src="~img/opposite-arrow.png" alt="Gobit table header arrow" class="img-fluid position-absolute" style="top:17px;">
+            </th>
+            <th class="position-relative">
+              현재가
+              <img src="~img/double-arrow.png" alt="Gobit table header arrow" class="img-fluid position-absolute" style="top:17px;">
+            </th>
+            <th class="position-relative">
+              전일대비
+              <img src="~img/double-arrow.png" alt="Gobit table header arrow" class="img-fluid position-absolute" style="top:17px;">
+            </th>
+            <th class="position-relative">
+              거래대금
+              <img src="~img/double-arrow.png" alt="Gobit table header arrow" class="img-fluid position-absolute" style="top:17px;">
+            </th>
+          </tr>
+          <tr class="exchange-table-body-row" :key="index" v-for="index in 5">
+            <td class="position-relative rt">
+              &uarr;999
+            </td>
+            <td>
+              비트코인캐시
+              <span>
+                BTC/KRW
+              </span>
+            </td>
+            <td class="rt">
+              369,650
+            </td>
+            <td class="rt">
+              100.55%
+              <span>
+                5,650
+              </span>
+            </td>
+            <td>
+              137,816 천원
+            </td>
+          </tr>
+          <tr class="exchange-table-body-row" :key="index" v-for="index in 5">
+            <td class="position-relative bt">
+              &uarr;999
+            </td>
+            <td>
+              비트코인에스브이
+              <span>
+                GRS/KRW
+              </span>
+            </td>
+            <td class="bt">
+              5,896,000
+            </td>
+            <td class="bt">
+              -0.40%
+              <span>
+                2.00
+              </span>
+            </td>
+            <td>
+              11,414 천원
+            </td>
+          </tr>
         </table>
       </div>
     </div>
