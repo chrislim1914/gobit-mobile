707f7c8d60ab12d49535a8d427002f19e307548a
chris lim <lm.chrstphr.m@gmail.com>
2019-05-06 13:27:16 +0800

    update on CoinTransaction.vue

--- src/components/DepositWithdraw/Index.vue
+++ src/components/DepositWithdraw/Index.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="app-depositwithdraw">
-    <!-- <coinTransaction></coinTransaction> -->
     <topbar></topbar>
     <depositwithdraw></depositwithdraw>
+    <!-- <coinTransaction></coinTransaction> -->
     <router-view></router-view>
   </div>
 </template>
