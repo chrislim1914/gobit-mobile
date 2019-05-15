8234bdf8ff848a9ff1edb942e288f4d7565eb8bc
Daniel Sarabusing <daniels.uth702@gmail.com>
2019-05-15 17:01:01 +0800

    updated trading lone tab 1 right section, trading lone tab 1 left section [WIP]

--- src/components/Trading/LinkOne.vue
+++ src/components/Trading/LinkOne.vue
@@ -34,11 +34,9 @@
                   </a>
                 </li>
                 <li class="nav-item position-relative">
-                  <div id="v-divide-1"></div>
                   <a class="nav-link font-weight-bold" id="two" data-toggle="tab" href="#two" role="tab" aria-controls="two" aria-selected="false">
                     매도
                   </a>
-                  <div id="v-divide-2"></div>
                 </li>
                 <li class="nav-item">
                   <a class="nav-link font-weight-bold" id="three" data-toggle="tab" href="#three" role="tab" aria-controls="three" aria-selected="false">
@@ -47,9 +45,123 @@
                 </li>
               </ul>
               <div class="tab-content" id="myTabContent">
-                <div class="tab-pane fade show active" id="one" role="tabpanel" aria-labelledby="one-tab">...</div>
-                <div class="tab-pane fade" id="two" role="tabpanel" aria-labelledby="two-tab">...</div>
-                <div class="tab-pane fade" id="three" role="tabpanel" aria-labelledby="three-tab">...</div>
+                <div class="tab-pane fade show active" id="one" role="tabpanel" aria-labelledby="one-tab">
+                  <ul id="trading-lone-tab-1" class="my-0">
+                    <li>
+                      구분
+                      <img src="~img/info32x32.png" alt="Gobit info" class="img-fluid">
+                    </li>
+                    <li>
+                      <input type="checkbox" class="option-input radio" name="example" />
+                      지정가
+                    </li>
+                    <li>
+                      <input type="checkbox" class="option-input radio" name="example" />
+                      시장가
+                    </li>
+                  </ul>
+                  <div id="trading-lone-tab-1-div-1">
+                    <h6 id="trading-lone-tab-1-text-1" class="mb-0 font-weight-bold">
+                      주문가능
+                    </h6>
+                    <h6 id="trading-lone-tab-1-text-2" class="mb-0">
+                      0 KRW
+                    </h6>
+                  </div>
+                  <div id="trading-lone-tab-1-div-2">
+                    <div id="trading-lone-tab-1-div-2-subdiv-1">
+                      <h6 class="mb-0">
+                        수량
+                      </h6>
+                      <h6 class="mb-0">
+                        0
+                      </h6>
+                    </div>
+                    <div id="trading-lone-tab-1-div-2-subdiv-2">
+                      <div class="dropdown">
+                        <a class="btn dropbtn font-weight-bold">
+                          가능
+                          <img src="~img/drop-down-arrow16x16#7e.png" alt="Gobit drop down icon 16x16" class="pl-4 img-fluid">
+                        </a>
+                        <div class="dropdown-content">
+                          <a href="#">75%</a>
+                          <a href="#">50%</a>
+                          <a href="#">25%</a>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div id="trading-lone-tab-1-div-3">
+                    <div id="trading-lone-tab-1-div-3-subdiv-1">
+                      <h6 class="mb-0">
+                        가격
+                      </h6>
+                      <h6 class="mb-0">
+                        190,200
+                      </h6>
+                    </div>
+                    <div id="trading-lone-tab-1-div-3-subdiv-2">
+                      <a href="#" class="btn">-</a><a href="#" class="btn">+</a>
+                    </div>
+                  </div>
+                  <div id="trading-lone-tab-1-div-4">
+                    <div class="dropdown">
+                      <a class="btn dropbtn font-weight-bold">
+                        현재가 대비 %
+                        <img src="~img/drop-down-arrow16x16#7e.png" alt="Gobit drop down icon 16x16" class="pl-4 img-fluid">
+                      </a>
+                      <div class="dropdown-content">
+                        <a href="#">75%</a>
+                        <a href="#">50%</a>
+                        <a href="#">25%</a>
+                      </div>
+                    </div>
+                  </div>
+                  <div id="trading-lone-tab-1-div-5">
+                    <h6 class="mb-0">
+                      총액
+                    </h6>
+                    <h6 class="mb-0">
+                      0 KRW
+                    </h6>
+                  </div>
+                  <div id="trading-lone-tab-1-div-6">
+                    <button id="trading-lone-tab-1-div-6-button-1" class="btn">
+                      초기화
+                    </button><button id="trading-lone-tab-1-div-6-button-2" class="btn">
+                      매수
+                    </button>
+                  </div>
+                  <div id="trading-lone-tab-1-div-7">
+                    <div id="trading-lone-tab-1-div-7-sub-1">
+                      <h6 class="mb-0">
+                        최소주문금액
+                      </h6>
+                      <h6 class="mb-0">
+                        500 KRW
+                      </h6>
+                    </div>
+                    <div id="trading-lone-tab-1-div-7-sub-2">
+                      <h6 class="mb-0">
+                        수수료(부가세포함)
+                      </h6>
+                      <h6 class="mb-0">
+                        0.05%
+                      </h6>
+                    </div>
+                  </div>
+                  <div id="trading-lone-tab-1-div-8">
+                    <button class="btn">
+                      호가 주문
+                    </button>
+                  </div>
+                </div>
+                <div class="tab-pane fade" id="two" role="tabpanel" aria-labelledby="two-tab">
+
+                </div>
+                <div class="tab-pane fade" id="three" role="tabpanel" aria-labelledby="three-tab">
+                  
+                </div>
               </div>
             </div>
           </div>
