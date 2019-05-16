4d64d780b84951e6f1ed77da1fc0762cbf388ea0
Adrianne Peredo <aperedo@uth702@gmail.com>
2019-05-16 17:49:23 +0800

    Update exchange page

--- src/components/Exchange/Search.vue
+++ src/components/Exchange/Search.vue
@@ -4,7 +4,7 @@
     <div id="exchange-search-holder">
       <div id="exchange-search-content">
         <label class="sr-only" for="inlineFormInputGroup">코인명 검색</label>
-        <div class="input-group">
+        <div id="search-input-group" class="input-group">
           <div class="input-group-prepend">
             <div class="input-group-text px-0 py-0">
               <img src="~img/search32x32.png" alt="Gobit search icon" id="search-icon" class="img-fluid">
